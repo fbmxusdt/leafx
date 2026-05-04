e1edc4f5ad794f195ac850488e138bd80d5f22a4
fbmxusdt <fbmxtoken@gmail.com>
2026-05-04 23:29:30 +0800

    Updates

---
+++ assets/all-wallets-BATLWaO5.js
@@ -0,0 +1,6 @@
+import{w as l}from"./core-DjWAj78y.js";import"./index-CQR_aN9A.js";import"./index.es-C7mYpEPc.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
